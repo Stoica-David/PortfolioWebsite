f23926a37519d1b34b7d8b9d564aa8c3785491ba
Stoica-David <stoicadavidioan@gmail.com>
2024-12-23 15:08:13 +0200

    Added filtering to the project.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { ProjectsService } from '../_services/projects.service';
+import { Tag } from '../_models/tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -11,11 +12,90 @@ import { ProjectsService } from '../_services/projects.service';
 export class PortfolioComponent implements OnInit {  
   projects = {} as Project[];
 
+  isCollapsed: boolean = true;
+
+  filtering: boolean = false;
+
+  typeScript: boolean = false;
+  angular: boolean = false;
+  python: boolean = false;
+  csharp: boolean = false;
+  java: boolean = false;
+  nodejs: boolean = false;
+  aspnet: boolean = false;
+  javascript: boolean = false;
+  react: boolean = false;
+
   constructor(private titleService: Title, private projectService: ProjectsService){
     this.titleService.setTitle('Stoica David Ioan - Portfolio');
   }
 
   ngOnInit(): void {
+    this.projects = this.projectService.GetProjects();
+  }
+
+  Filter() {
+    let filterTags: Tag[] = [];
+
+    if (this.typeScript) {
+      filterTags.push(Tag.TYPESCRIPT);
+    }
+
+    if (this.angular) {
+      filterTags.push(Tag.ANGULAR);
+    }
+
+    if (this.python) {
+      filterTags.push(Tag.PYTHON);
+    }
+
+    if (this.csharp) {
+      filterTags.push(Tag.CSHARP);
+    }
+
+    if (this.java) {
+      filterTags.push(Tag.JAVA);
+    }
+    
+    if (this.nodejs) {
+      filterTags.push(Tag.NODEJS);
+    }
+
+    if (this.aspnet) {
+      filterTags.push(Tag.ASPNET);
+    }
+
+    if (this.javascript) {
+      filterTags.push(Tag.JAVASCRIPT);
+    }
+
+    if (this.react) {
+      filterTags.push(Tag.REACT);
+    }
+
+    if (this.python || this.csharp || this.java || this.angular || this.typeScript || this.nodejs || this.aspnet || this.javascript || this.react) {
+      this.filtering = true;
+    }
+    else {
+      this.filtering = false;
+    }
+
+    this.projects = this.projectService.GetProjectsByFilter(filterTags);
+  }
+
+  ResetFilters() {
+    this.python = false;
+    this.csharp = false;
+    this.java = false;
+    this.javascript = false;
+    this.angular = false;
+    this.aspnet = false;
+    this.nodejs = false;
+    this.react = false;
+    this.typeScript = false;
+
+    this.filtering = false;
+
     this.projects = this.projectService.GetProjects();
   }
 }
